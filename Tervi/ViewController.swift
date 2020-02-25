54390f9d34f2dc422db536a19440abec6268ac19
GeorgeLiu <george_liu@addcn.com>
2020-02-25 11:11:02 +0800

    [Refactor] refactor create cell view function

--- Tervi/ViewController.swift
+++ Tervi/ViewController.swift
@@ -22,38 +22,56 @@ class ViewController: UIViewController {
 //        stackView.isLayoutMarginsRelativeArrangement = true
 //        stackView.layoutMargins = UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
         
-        for column in 0 ... columns {
-            let subStackView = BoarderStackView {
-                $0.axis = .vertical
-                $0.distribution = .fillEqually
-            }
-            for _ in 0 ... rows {
-                let baseView = CellView {
-                    switch column % 4 {
-                    case 0:
-                        $0.setColor(color: .blue)
-                    case 1:
-                        $0.setColor(color: .red)
-                    case 2:
-                        $0.setColor(color: .yellow)
-                    case 3:
-                        $0.setColor(color: .brown)
-                    default:
-                        fatalError()
-                    }
-                }
-                subStackView.addArrangedSubview(baseView)
-            }
-            
-            let buttonView = ButtonView {
-                $0.confirmBtn.addTarget(self, action: #selector(tapConfirmBtn(_:)), for: .touchUpInside)
-            }
-            subStackView.addArrangedSubview(buttonView)
-            stackView.addArrangedSubview(subStackView)
-        }
-        
+        renderUI()
         let sub = stackView.arrangedSubviews[2] as! BoarderStackView
         sub.addBoarder(color: .focus)
+    }
+    
+    private func renderUI() {
+        for column in 0 ... columns {
+            stackView.addArrangedSubview(createColumnView(column: column))
+        }
+    }
+    
+    private func createColumnView(column: Int) -> BoarderStackView {
+        let subStackView = BoarderStackView {
+            $0.axis = .vertical
+            $0.distribution = .fillEqually
+        }
+        
+        let isFinishedAddingCell = isFinishedAddCellViews(into: subStackView, column: column)
+        addBottomView(into: subStackView, isFinishedAddingCell: isFinishedAddingCell)
+        
+        return subStackView
+    }
+    
+    private func isFinishedAddCellViews(into stackview: BoarderStackView, column: Int) -> Bool {
+        for _ in 0 ... rows {
+            let baseView = CellView {
+                switch column % 4 {
+                case 0:
+                    $0.setColor(color: .blue)
+                case 1:
+                    $0.setColor(color: .red)
+                case 2:
+                    $0.setColor(color: .yellow)
+                case 3:
+                    $0.setColor(color: .brown)
+                default:
+                    fatalError()
+                }
+            }
+            stackview.addArrangedSubview(baseView)
+        }
+        return true
+    }
+    
+    private func addBottomView(into stackview: BoarderStackView, isFinishedAddingCell: Bool) {
+        guard isFinishedAddingCell else { return }
+        let buttonView = ButtonView {
+            $0.confirmBtn.addTarget(self, action: #selector(tapConfirmBtn(_:)), for: .touchUpInside)
+        }
+        stackview.addArrangedSubview(buttonView)
     }
     
     @objc func tapConfirmBtn(_ sender: UIButton) {
